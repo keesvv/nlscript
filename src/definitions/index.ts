7d1053da80f42e190ccbf6f25fa9c968b92baad1
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-08 20:23:20 +0100

    Add 'toon' instruction

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,8 +1,9 @@
 import InstructionDefinition from '../InstructionDefinition';
 
 import maak from './maak.handler';
+import toon from './toon.handler';
 
 export default new Array<InstructionDefinition>(
-  new InstructionDefinition('maak', maak)
+  new InstructionDefinition('maak', maak),
+  new InstructionDefinition('toon', toon)
 );
-
---
+++ src/definitions/toon.handler.ts
@@ -0,0 +1,6 @@
+import Instruction from '../Instruction';
+import storage from '../storage';
+
+export default (i: Instruction) => {
+  console.log(storage.get(i.args[0]));
+};
