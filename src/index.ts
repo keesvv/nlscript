1940f58c68b59667b10c624eb62c2d3ba7cac527
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-08 20:32:27 +0100

    Move index.ts -> src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import Parser from './src/Parser';
+import Parser from './Parser';
 
 const content = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 const parser = new Parser(content);
