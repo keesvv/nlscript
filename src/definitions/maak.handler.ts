406bf76042ada77dc57ab2a335d30321ec5740d0
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-08 20:19:27 +0100

    Add functionality to 'maak' handler

--- src/definitions/maak.handler.ts
+++ src/definitions/maak.handler.ts
@@ -1,5 +1,6 @@
 import Instruction from '../Instruction';
+import storage from '../storage';
 
 export default (i: Instruction) => {
-  console.log(i.args);
+  storage.set(i.args[0], i.args[1]);
 };
