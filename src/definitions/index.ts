8882dffd6167eceab581a5b4f03fa586746b1aa9
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-09 17:03:35 +0100

    Remove old definitions file

--- src/definitions/index.ts
+++
@@ -1,9 +0,0 @@
-import InstructionDefinition from '../InstructionDefinition';
-
-import maak from './maak.handler';
-import toon from './toon.handler';
-
-export default new Array<InstructionDefinition>(
-  new InstructionDefinition('maak', maak),
-  new InstructionDefinition('toon', toon)
-);
