7b31e582cc6733763fe71df291c54909f3d0d3b7
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-09 20:26:55 +0100

    Add scherm.wis instruction

--- src/modules/scherm/index.ts
+++ src/modules/scherm/index.ts
@@ -2,11 +2,13 @@ import InstructionDefinition from '../../InstructionDefinition';
 import Module from '../../Module';
 
 import toon from './toon.handler';
+import wis from './wis.handler';
 
 export default new Module({
   name: 'Scherm',
   keyword: 'scherm',
   definitions: [
-    new InstructionDefinition('toon', toon)
+    new InstructionDefinition('toon', toon),
+    new InstructionDefinition('wis', wis)
   ]
 })
