20265b2ea0a35087397d8c24c0fd26d3cc36ab77
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-10 16:41:18 +0100

    Made 'filename' optional

--- src/interfaces/IErrorContext.ts
+++ src/interfaces/IErrorContext.ts
@@ -1,7 +1,7 @@
 /* eslint-disable semi */
 /* eslint-disable no-extra-semi */
 export default interface IErrorContext {
-  filename: string;
+  filename?: string;
   line: string;
   error: string;
 };
