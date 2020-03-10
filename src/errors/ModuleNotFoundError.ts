7f7b012c291931c0ac0d12e37cab0696b90026ec
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-10 16:29:25 +0100

    Add moduleName as param

--- src/errors/ModuleNotFoundError.ts
+++ src/errors/ModuleNotFoundError.ts
@@ -2,8 +2,8 @@ import UserError from './UserError';
 import IErrorContext from '../interfaces/IErrorContext';
 
 class ModuleNotFoundError extends UserError {
-  constructor(context: IErrorContext) {
-    super('Module not found.', context);
+  constructor(moduleName: string, context: IErrorContext) {
+    super(`Module not found: '${moduleName}'.`, context);
   }
 }
 
