01e410375c8e9add4502f1011e7af065481dbb9a
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-13 09:33:39 +0100

    Register 'globaal' module by default & add comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import Parser from './Parser';
 import Engine from './Engine';
+import Module from './Module';
 import Preprocessor from './Preprocessor';
+import globalModule from './modules/src/modules/globaal';
+
+// Register the 'globaal' module by default
+Module.registerModules(globalModule);
 
 (async () => {
+  // Get the contents of the specified file
   const content = await Preprocessor.processFile(process.argv[2]);
 
   const parser = new Parser(content);
   let instructions;
 
   try {
+    // Parse content into an array of instructions
     instructions = parser.parse();
   } catch (error) {
     if (error.isUserError) {
@@ -23,5 +30,6 @@ import Preprocessor from './Preprocessor';
     process.exit(1);
   }
 
+  // Execute all instructions in a sequence
   await Engine.execute(instructions);
 })();
