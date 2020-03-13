c68cd1c0037fa4d145eaa87d5cc19fd1cc3697f8
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-13 08:44:43 +0100

    Set mode according to environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   target: 'node',
   entry: './src/index.ts',
   module: {
