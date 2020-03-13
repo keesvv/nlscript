922d20654999c95775b90f40b188e2947223a711
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-13 11:54:48 +0100

    Add ModuleNotRegisteredError class

---
+++ src/errors/ModuleNotRegisteredError.ts
@@ -0,0 +1,10 @@
+import UserError from './UserError';
+import IErrorContext from '../interfaces/IErrorContext';
+
+class ModuleNotRegisteredError extends UserError {
+  constructor(moduleName: string, context: IErrorContext) {
+    super(`Module not registered: '${moduleName}'. Register it using \`gebruik ${moduleName};\``, context);
+  }
+}
+
+export default ModuleNotRegisteredError;
