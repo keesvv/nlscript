a8e929451138c42c4801273ccaca4a96b3683f25
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-03-13 12:01:26 +0100

    Add full stop at the end of the sentence

--- src/errors/ModuleNotRegisteredError.ts
+++ src/errors/ModuleNotRegisteredError.ts
@@ -3,7 +3,7 @@ import IErrorContext from '../interfaces/IErrorContext';
 
 class ModuleNotRegisteredError extends UserError {
   constructor(moduleName: string, context: IErrorContext) {
-    super(`Module not registered: '${moduleName}'. Register it using \`gebruik ${moduleName};\``, context);
+    super(`Module not registered: '${moduleName}'. Register it using \`gebruik ${moduleName};\`.`, context);
   }
 }
 
